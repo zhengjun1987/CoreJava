f8c9749719c6b0aacf5c48c9b4187875fd9f095a
Zheng Jun <zhengjun1987@outlook.com>
2018-03-21 18:09:33 +0800

    Core Java advanced chapter03 基本程序设计结构 2018-3-21 18:09:24

---
+++ fundamental/src/chapter03basical_program_structure/JavaConstants.java
@@ -0,0 +1,14 @@
+package chapter03basical_program_structure;
+
+/**
+ * Author: Zheng Jun
+ * Mail:zhengjun1987@outlook.com
+ * Date: 2018/3/19 15:28
+ */
+public class JavaConstants {
+    public static final double CM_PER_INCH = 2.54;//类常量
+
+    public static void main(String[] args) {
+        final double PI = 3.1416;//常量
+    }
+}
